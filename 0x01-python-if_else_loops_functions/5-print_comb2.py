d3aa1003f6820603a135af8f7801c0a2e3b8aab9
FEzzKm <elkacem.fatima.ezzahra@gmail.com>
2023-10-04 16:33:24 -0700

    5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for a in range(99):
-    print("{:02}".format(a), end=', ')
-print()
+for a in range(100):
+    if a == 99:
+        print("{:02}".format(a))
+    else:
+        print("{:02}".format(a), end=', ')
