f6f3e1c9c3560055e6b0dbdf61b35190fa748eed
FEzzKm <elkacem.fatima.ezzahra@gmail.com>
2023-10-04 17:47:36 -0700

    12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for a in range(101):
+    for a in range(1,101):
         if a % 3 == 0 and a % 5 == 0:
-            print('FizzBuzz')
+            print('FizzBuzz', end=' ')
         elif a % 3 == 0:
             print('Fizz')
         elif a % 5 == 0:
