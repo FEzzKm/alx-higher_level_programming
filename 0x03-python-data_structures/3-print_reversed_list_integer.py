5b1b9f3f4a5ca827b7008a04c3abd431308b02a9
FEzzKm <elkacem.fatima.ezzahra@gmail.com>
2023-10-09 16:01:58 -0700

    3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-""" reverse list """
+""" fct prints ints in reverse"""
+
 
 def print_reversed_list_integer(my_list=[]):
     if not my_list:
         pass
     else:
-        a = len(my_list) - 1
-       while num >= 0:
-           print("{:d}".format(my_list[a]))
+       a = len(my_list) - 1
+
+       while a >= 0:
+            print("{:d}".format(my_list[a]))
             a -= 1
