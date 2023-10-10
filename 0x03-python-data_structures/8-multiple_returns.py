69a98f8330959a06fb2b253edc39266fdfb90948
FEzzKm <elkacem.fatima.ezzahra@gmail.com>
2023-10-10 12:55:28 -0700

    8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    """ a function that returns a tuple with the length of a string and its first character """
+    """ a function that returns a tuple """
     if len(sentence) == 0:
         return (0, None)
     else:
