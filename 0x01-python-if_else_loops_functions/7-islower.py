906860be92ed35cb078746703c1e8554b0036b32
FEzzKm <elkacem.fatima.ezzahra@gmail.com>
2023-10-11 13:13:50 -0700

    78

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-def is_lower(c):
-    return (ord('a') <= ord(c) <= ord('z'))
+def islower(c):
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
