7a8bbdec435e3cd8ada54c94b5cd32e18b0d9d8f
FEzzKm <elkacem.fatima.ezzahra@gmail.com>
2023-10-11 15:44:35 -0700

    2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     elif a == 1:
         print("1 argument:")
     else:
-        print("{} arguments:".format(count))
-    for i in range(count):
+        print("{} arguments:".format(a))
+    for i in range(a):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
