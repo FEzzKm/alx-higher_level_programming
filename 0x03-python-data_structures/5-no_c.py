819e5e06d6193aca6bf3b087f1d12bc1bb59874d
FEzzKM <elkacem.fatima.ezzahra@gmail.com>
2024-01-07 13:35:28 -0800

    5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,17 +1,4 @@
 #!/usr/bin/python3
-
-"""Function removes c and C from string"""
-
-
 def no_c(my_string):
-    string_length = len(my_string)
-
-    i = 0
-    ns = my_string[:]
-
-    for char in range(string_length):
-        if (my_string[char] == 'c' or my_string[char] == 'C'):
-            ns = new_string[:(char - i)] + my_string[(char + 1):]
-            i += 1
-
-    return (ns)
+    new_string = my_string.translate({ord(i): None for i in 'cC'})
+    return new_string
